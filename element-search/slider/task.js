565a6fc9a2ab783cd4bfc7f6761a28e198151c88
Vito <example@gmail.com>
2024-09-27 10:41:16 +0600

    Code rewritten

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,20 +1,20 @@
 const slidingImg = [...document.querySelectorAll('.slider__item')];
-const slidingArrows = [...document.querySelectorAll('.slider__arrow')];
+const prevArrow = document.querySelector('.slider__arrow_prev');
+const nextArrow = document.querySelector('.slider__arrow_next');
 
 let currentImg = 0;
 
-const sliderSelector = (event) => {
-    const item = event.target;
-    if (item.classList.contains('slider__arrow_next')) {
-        slidingImg[currentImg].classList.remove('slider__item_active');
-        currentImg = (currentImg + 1) % slidingImg.length;
-        slidingImg[currentImg].classList.add('slider__item_active');
+const prevSelector = () => {
+    slidingImg[currentImg].classList.remove('slider__item_active');
+    currentImg = (currentImg - 1 + slidingImg.length) % slidingImg.length;
+    slidingImg[currentImg].classList.add('slider__item_active');
     }
-    if (item.classList.contains('slider__arrow_prev')) {
-        slidingImg[currentImg].classList.remove('slider__item_active');
-        currentImg = (currentImg - 1 + slidingImg.length) % slidingImg.length;
-        slidingImg[currentImg].classList.add('slider__item_active');
-    }
+
+const nextSelector = () => {
+    slidingImg[currentImg].classList.remove('slider__item_active');
+    currentImg = (currentImg + 1) % slidingImg.length;
+    slidingImg[currentImg].classList.add('slider__item_active');
 }
 
-slidingArrows.forEach(arrow => arrow.addEventListener('click', sliderSelector));
+prevArrow.addEventListener('click', prevSelector);
+nextArrow.addEventListener('click', nextSelector);
