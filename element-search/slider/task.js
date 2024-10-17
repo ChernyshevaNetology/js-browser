fcc2aa0363d5f3c7a828b10569b71a8f8b0e94c5
ChernyshevaNetology <chernyshevak87@mail.ru>
2024-10-17 17:11:59 +0200

    fix/fix code

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,16 +6,16 @@ const nextArrow = document.querySelector('.slider__arrow_next');
 
 let currentImg = 0;
 let currentDot = 0; // зачем тебе currentDot? Если currentImg - будет по тому же индексу?
-// dots[currentDot].classList.add('slider__dot_active');
+// dots[currentDot].classList.add('slider__dot_active'); раскометируй когда точки динамически отрисуешь
 
 const activeStatusRemoved = () => {
     slidingImg[currentImg].classList.remove('slider__item_active');
-    // dots[currentDot].classList.remove('slider__dot_active'); // излишне
+    // dots[currentDot].classList.remove('slider__dot_active'); раскометируй когда точки динамически отрисуешь
     }
 
 const activeStatusAdded = () => {
     slidingImg[currentImg].classList.add('slider__item_active');
-    // dots[currentDot].classList.add('slider__dot_active'); // это не надо ты строчкой выше уже все вычислил, здесь тоже самое
+    // dots[currentDot].classList.add('slider__dot_active'); раскометируй когда точки динамически отрисуешь
 }
 
 const dotClicker = (event) => {
@@ -24,12 +24,12 @@ const dotClicker = (event) => {
 
 const nextCounter = () => {
     currentImg = (currentImg + 1) % slidingImg.length;
-    // currentDot = (currentDot + 1) % dots.length;
+    // currentDot = (currentDot + 1) % dots.length; // это не надо ты строчкой выше уже все вычислил, здесь тоже самое
 }
 
 const prevCounter = () => {
     currentImg = (currentImg - 1 + slidingImg.length) % slidingImg.length;
-    // currentDot = (currentDot - 1 + dots.length) % dots.length;
+    // currentDot = (currentDot - 1 + dots.length) % dots.length; // излишне
 }
 
 const prevSelector = () => {
