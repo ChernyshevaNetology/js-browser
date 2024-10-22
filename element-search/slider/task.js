a218109d9d0830058d7fcac0eeb8f391e25150db
Vito <example@gmail.com>
2024-10-22 23:01:07 +0600

    Some bugs fixed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,44 +1,48 @@
 const slidingImg = [...document.querySelectorAll('.slider__item')];
-const dots = document.querySelector('.slider__dots');
+const dotsContainer = document.querySelector('.slider__dots');
 const prevArrow = document.querySelector('.slider__arrow_prev');
 const nextArrow = document.querySelector('.slider__arrow_next');
 
 let currentImg = 0;
 const count = slidingImg.length;
-// dots[currentImg].classList.add('slider__dot_active'); // раскометируй когда точки динамически отрисуешь
-
-const activeStatusRemoved = () => {
-    slidingImg[currentImg].classList.remove('slider__item_active');
-    dots[currentImg].classList.remove('slider__dot_active'); // раскометируй когда точки динамически отрисуешь
-    }
-
-const activeStatusAdded = () => {
-    slidingImg[currentImg].classList.add('slider__item_active');
-    dots[currentImg].classList.add('slider__dot_active'); // раскометируй когда точки динамически отрисуешь
-}
 
 const createDotElem = (idx) => {
     const dotElem = document.createElement('div');
     dotElem.classList.add('slider__dot');
     dotElem.setAttribute('data-index', idx);
+    if(idx === 0) {
+        dotElem.classList.add('slider__dot_active')
+    }
+
     return dotElem;
 }
 
 for (let i = 0; i < count; i++) {
-    dots.appendChild(createDotElem(i));
-    }
+    dotsContainer.appendChild(createDotElem(i));
+}
+const dots = [...document.querySelectorAll('slider__dot')];
 
 const handleDotClick = (event) => {
-const index = +event.target.getAttribute('data-index');
-console.log('index', index);
+    const index = +event.target.getAttribute('data-index');
+    dots[index].classList.add('slider__dot_active');
 }
 
 const nextCounter = () => {
     currentImg = (currentImg + 1) % slidingImg.length;
-    }
+}
 
 const prevCounter = () => {
     currentImg = (currentImg - 1 + slidingImg.length) % slidingImg.length;
+}
+
+const activeStatusRemoved = () => {
+    slidingImg[currentImg].classList.remove('slider__item_active');
+    //dots[currentImg].classList.remove('slider__dot_active');
+    }
+
+const activeStatusAdded = () => {
+    slidingImg[currentImg].classList.add('slider__item_active');
+    //dots[currentImg].classList.add('slider__dot_active');
     }
 
 const prevSelector = () => {
@@ -55,4 +59,4 @@ const nextSelector = () => {
 
 prevArrow.addEventListener('click', prevSelector);
 nextArrow.addEventListener('click', nextSelector);
-dots.addEventListener('click', handleDotClick);
+dotsContainer.addEventListener('click', handleDotClick);
