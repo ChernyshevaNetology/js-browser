55141bf8d9b595a32a16c1451e4c978cb0c890cc
Vito <example@gmail.com>
2024-10-28 20:56:37 +0600

    Code fixed

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,19 +1,14 @@
 const button = document.querySelector('.dropdown__value');
 const menuItems = document.querySelector('.dropdown__list');
 
-const handleButtonExpansion = (event) => {
-    const buttonExpanded = event.target.nextElementSibling;
-    if(buttonExpanded) {
-        event.preventDefault();
-        buttonExpanded.classList.toggle('dropdown__list_active');
+const handleButtonExpansion = () => {
+    menuItems.classList.toggle('dropdown__list_active');
     }
-}
 
 const handleButtonClickAction = (event) => {
-    const menuItem = event.target;
-    if(menuItem) {
-        button.textContent = menuItem.textContent;
-    }
+    event.preventDefault();
+    button.textContent = event.target.textContent;
+    menuItems.classList.toggle('dropdown__list_active');
 }
 
 button.addEventListener('click', handleButtonExpansion);
