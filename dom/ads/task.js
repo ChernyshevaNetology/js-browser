8200bb93d89843b3073f0f9bb36bd829aa8925a1
Vito <example@gmail.com>
2024-11-13 17:41:07 +0600

    Final changes made

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,23 +1,16 @@
-const rotatorArray = [...document.querySelectorAll('.rotator .rotator__case')];
-const rotatorLength = rotatorArray.length;
-
-let currentIdx = 0;
-
-const handleIncrementAction = () => {
-    currentIdx = (currentIdx + 1) % rotatorLength;
+const handleDisplayNextAd = (rotator, timeOut) => {
+    const rotatorBlock = [...document.querySelectorAll(rotator)];
+    const rotatorLength = rotatorBlock.length;
+    let currentIdx = 0;
+    setInterval(() => {
+        rotatorBlock[currentIdx].classList.remove('rotator__case_active');
+        currentIdx = (currentIdx + 1) % rotatorLength;
+        rotatorBlock[currentIdx].classList.add('rotator__case_active');
+    }, timeOut)
 }
 
-const handleDeactivationStatus = () => {
-    rotatorArray[currentIdx].classList.remove('rotator__case_active');
-}
-
-const handleActivationStatus = () => {
-    rotatorArray[currentIdx].classList.add('rotator__case_active');
-}
-
-setInterval(() => {
-    handleDeactivationStatus();
-    handleIncrementAction(currentIdx);
-    handleActivationStatus();
-}, 1000)
-ы
+handleDisplayNextAd ('.rotator .rotator__case', 1000);
+handleDisplayNextAd ('.rotator2 .rotator__case', 1000);
+handleDisplayNextAd ('.rotator3 .rotator__case', 1000);
+handleDisplayNextAd ('.rotator4 .rotator__case', 1000);
+handleDisplayNextAd ('.rotator5 .rotator__case', 1000);
