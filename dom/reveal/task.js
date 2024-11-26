78476b445b5878b5cf902359a362f8fe996a1b31
Vito <example@gmail.com>
2024-11-26 20:13:58 +0600

    Code improved

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,16 +2,16 @@ const bannerBlocks = document.querySelectorAll('.reveal');
 
 const bannerObserver = (entries, observer) => {
     entries.forEach((banner) => {
-        if(banner.target.classList.contains('reveal')) {
-            banner.target.classList.add('reveal_active')
-            observer.unobserve(banner.target)
+        if (banner.isIntersecting) {
+            banner.target.classList.add('reveal_active');
+            observer.unobserve(banner.target);
         }
-        })
+    })
 }
 
 const configs = {
     rootMargin: '0px',
-    threshold: 0,
+    threshold: 0.5,
 }
 
 const observer = new IntersectionObserver(bannerObserver, configs);
