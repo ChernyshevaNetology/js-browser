216b3c57c0953bb75bc763c1f5bfba1293267f2b
ChernyshevaNetology <chernyshevak87@mail.ru>
2024-12-12 19:41:23 +0200

    feat: code structure added

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,6 +3,20 @@ const textSizeBlock = document.querySelector('.book__control_font-size');
 const textColorBlock = document.querySelector('.book__control_color');
 const backgroundColorBlock = document.querySelector('.book__control_background');
 
+const controlAttribute = {
+        /**
+        / придумай архитектуру как ты
+         будешь хранить и по ключу получать нужные тебе аотрибуты
+         */
+}
+
+
+
+/**
+ * Это динамическая функция,
+ * она может работать с переданными ей аргументами
+ * Она прекрасна!
+ */
 const handleActiveIconClassChange = (domElement, activeClass, key, classPrefix) => {
         const lineWithEnabledClass = domElement.querySelector('.' + activeClass);
         lineWithEnabledClass.classList.remove(activeClass);
@@ -13,6 +27,25 @@ const handleActiveIconClassChange = (domElement, activeClass, key, classPrefix) 
         }
 }
 
+/**
+ * У нас должна быть всего ОДНА
+ * функция котороая задает книги активный класс
+ * все элементы в ней, она должна вычислять динамически
+ */
+const handleChangeActiveControlElement = (event) => {
+        event.preventDefault(); // так и остается
+        handleActiveIconClassChange(textSizeBlock, 'font-size_active', 'size', 'book_fs-'); // все эти параметры нужно вычислить передать динамически
+        event.target.classList.add('font-size_active'); // активный класс должен быть вычислен динамически
+        const dataSet = event.target.dataset.size; // ключ динамически (не size) а ключ, может быть size, textColor или bgColor или любой из тысячи!
+        if (dataSet) {
+                bookBlock.classList.add(`book_fs-${dataSet}`);
+        }
+}
+
+
+/**
+ * Этот код джуна удалишь потом
+ */
 const handleSizeSelection = (event) => {
         event.preventDefault();
         handleActiveIconClassChange(textSizeBlock, 'font-size_active', 'size', 'book_fs-');
