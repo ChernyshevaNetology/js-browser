ec2ca013cd7b80423a13aefa08cd1f4e6acb157c
Vito <example@gmail.com>
2024-12-12 22:37:51 +0600

    The code refractured and made functional

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,79 +1,49 @@
-const fontSizeLinks = document.querySelectorAll('.book .font-size');
-const colorLinks = document.querySelectorAll('.book__control_color a');
-const backgroundLinks = document.querySelectorAll('.book__control_background a');
 const bookBlock = document.querySelector('.book');
-const textBlock = document.querySelector('.book__control_color');
-const backgroundBlock = document.querySelector('.book__control_background');
+const textSizeBlock = document.querySelector('.book__control_font-size');
+const textColorBlock = document.querySelector('.book__control_color');
+const backgroundColorBlock = document.querySelector('.book__control_background');
 
-let prevSize = null;
-let prevTextColor = null;
-let prevBackgroundColor = null;
+const handleActiveIconClassChange = (domElement, activeClass, key, classPrefix) => {
+        const lineWithEnabledClass = domElement.querySelector('.' + activeClass);
+        lineWithEnabledClass.classList.remove(activeClass);
+        const keyValue = lineWithEnabledClass.dataset[key];
+        const classSelector = `${classPrefix + keyValue}`;
+        if(classSelector) {
+                bookBlock.classList.remove(classSelector);
+        }
+}
 
 const handleSizeSelection = (event) => {
         event.preventDefault();
-        fontSizeLinks.forEach((link) => {
-                if (link.classList.contains('font-size_active')) {
-                        prevSize = link.dataset.size;
-                        link.classList.remove('font-size_active');
-                }
-        })
-
+        handleActiveIconClassChange(textSizeBlock, 'font-size_active', 'size', 'book_fs-');
         event.target.classList.add('font-size_active');
-
-        if(prevSize) {
-                bookBlock.classList.remove(`book_fs-${prevSize}`);
-        }
-
         const dataSet = event.target.dataset.size;
-
         if (dataSet) {
                 bookBlock.classList.add(`book_fs-${dataSet}`);
         }
 }
 
-const handleColorSelection = (eventObject) => {
-        eventObject.preventDefault();
-        colorLinks.forEach((color) => {
-                if (color.classList.contains('color_active')) {
-                        prevTextColor = color.dataset.textColor;
-                        color.classList.remove('color_active');
-                }
-        })
-        eventObject.target.classList.add('color_active');
-
-        if(prevTextColor) {
-                bookBlock.classList.remove(`book_color-${prevTextColor}`);
-        }
-
-        const colorSet = eventObject.target.dataset.textColor;
-
+const handleColorSelection = (event) => {
+        event.preventDefault();
+        handleActiveIconClassChange(textColorBlock, 'color_active', 'textColor', 'book_color-');
+        event.target.classList.add('color_active');
+        const colorSet = event.target.dataset.textColor;
         if (colorSet) {
                 bookBlock.classList.add(`book_color-${colorSet}`);
 
         }
 }
 
-const handleBackgroundSelection = (e) => {
-        e.preventDefault();
-        backgroundLinks.forEach((bg) => {
-                if (bg.classList.contains('color_active')) {
-                        prevBackgroundColor = bg.dataset.bgColor;
-                        bg.classList.remove('color_active');
-                }
-        })
-        e.target.classList.add('color_active');
-
-        if(prevBackgroundColor) {
-                bookBlock.classList.remove(`book_bg-${prevBackgroundColor}`);
-        }
-
-        const backgroundSet = e.target.dataset.bgColor;
-
+const handleBackgroundSelection = (event) => {
+        event.preventDefault();
+        handleActiveIconClassChange(backgroundColorBlock, 'color_active', 'bgColor', 'book_bg-');
+        event.target.classList.add('color_active');
+        const backgroundSet = event.target.dataset.bgColor;
         if(backgroundSet) {
                 bookBlock.classList.add(`book_bg-${backgroundSet}`);
         }
 }
 
-bookBlock.addEventListener('click', handleSizeSelection);
-textBlock.addEventListener('click', handleColorSelection);
-backgroundBlock.addEventListener('click', handleBackgroundSelection);
+textSizeBlock.addEventListener('click', handleSizeSelection);
+textColorBlock.addEventListener('click', handleColorSelection);
+backgroundColorBlock.addEventListener('click', handleBackgroundSelection);
