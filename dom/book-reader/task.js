9245f09a3ab1b4c953949bedd8cb18b0839866ea
Vito <example@gmail.com>
2024-12-16 14:27:09 +0600

    Dynamic code - version 1.0

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,21 +2,37 @@ const bookBlock = document.querySelector('.book');
 const textSizeBlock = document.querySelector('.book__control_font-size');
 const textColorBlock = document.querySelector('.book__control_color');
 const backgroundColorBlock = document.querySelector('.book__control_background');
+const fontWeightBlock = document.querySelector('.book__control_boldness');
+const fontStyleBlock = document.querySelector('.book__control_font');
 
 const controlAttribute = {
-        /**
-        / придумай архитектуру как ты
-         будешь хранить и по ключу получать нужные тебе аотрибуты
-         */
+        size: {
+                domElement: 'textSizeBlock',
+                activeClass: 'font-size_active',
+                classPrefix: 'book_fs-'
+        },
+        textColor: {
+                domElement: 'textColorBlock',
+                activeClass: 'color_active',
+                classPrefix: 'book_color-'
+        },
+        bgColor: {
+                domElement: 'backgroundColorBlock',
+                activeClass: 'color_active',
+                classPrefix: 'book_bg-'
+        },
+        fontWeight: {
+                domElement: 'fontWeightBlock',
+                activeClass: 'font-weight_active',
+                classPrefix: 'font_weight-'
+        },
+        fontStyle: {
+                domElement: 'fontStyleBlock',
+                activeClass: 'font-style_active',
+                classPrefix: 'text-size_'
+        },
 }
 
-
-
-/**
- * Это динамическая функция,
- * она может работать с переданными ей аргументами
- * Она прекрасна!
- */
 const handleActiveIconClassChange = (domElement, activeClass, key, classPrefix) => {
         const lineWithEnabledClass = domElement.querySelector('.' + activeClass);
         lineWithEnabledClass.classList.remove(activeClass);
@@ -27,56 +43,22 @@ const handleActiveIconClassChange = (domElement, activeClass, key, classPrefix) 
         }
 }
 
-/**
- * У нас должна быть всего ОДНА
- * функция котороая задает книги активный класс
- * все элементы в ней, она должна вычислять динамически
- */
-const handleChangeActiveControlElement = (event) => {
+const handleChangeActiveControlElement = (event, object) => {
         event.preventDefault(); // так и остается
-        handleActiveIconClassChange(textSizeBlock, 'font-size_active', 'size', 'book_fs-'); // все эти параметры нужно вычислить передать динамически
-        event.target.classList.add('font-size_active'); // активный класс должен быть вычислен динамически
-        const dataSet = event.target.dataset.size; // ключ динамически (не size) а ключ, может быть size, textColor или bgColor или любой из тысячи!
+        const targetObject = event.target.dataset;
+        const keyFromArray = Object.keys(targetObject);
+        let key = keyFromArray[0];
+        handleActiveIconClassChange(controlAttribute[key].domElement, controlAttribute[key].activeClass, key, controlAttribute[key].classPrefix);
+        event.target.classList.add(controlAttribute[key].activeClass); // активный класс должен быть вычислен динамически
+        const dataSet = event.target.dataset[key];
+        // ключ динамически (не size) а ключ, может быть size, textColor или bgColor или любой из тысячи!
         if (dataSet) {
-                bookBlock.classList.add(`book_fs-${dataSet}`);
+                bookBlock.classList.add(`${controlAttribute[key].classPrefix + dataSet}`);
         }
 }
 
-
-/**
- * Этот код джуна удалишь потом
- */
-const handleSizeSelection = (event) => {
-        event.preventDefault();
-        handleActiveIconClassChange(textSizeBlock, 'font-size_active', 'size', 'book_fs-');
-        event.target.classList.add('font-size_active');
-        const dataSet = event.target.dataset.size;
-        if (dataSet) {
-                bookBlock.classList.add(`book_fs-${dataSet}`);
-        }
-}
-
-const handleColorSelection = (event) => {
-        event.preventDefault();
-        handleActiveIconClassChange(textColorBlock, 'color_active', 'textColor', 'book_color-');
-        event.target.classList.add('color_active');
-        const colorSet = event.target.dataset.textColor;
-        if (colorSet) {
-                bookBlock.classList.add(`book_color-${colorSet}`);
-
-        }
-}
-
-const handleBackgroundSelection = (event) => {
-        event.preventDefault();
-        handleActiveIconClassChange(backgroundColorBlock, 'color_active', 'bgColor', 'book_bg-');
-        event.target.classList.add('color_active');
-        const backgroundSet = event.target.dataset.bgColor;
-        if(backgroundSet) {
-                bookBlock.classList.add(`book_bg-${backgroundSet}`);
-        }
-}
-
-textSizeBlock.addEventListener('click', handleSizeSelection);
-textColorBlock.addEventListener('click', handleColorSelection);
-backgroundColorBlock.addEventListener('click', handleBackgroundSelection);
+textSizeBlock.addEventListener('click', handleChangeActiveControlElement);
+textColorBlock.addEventListener('click', handleChangeActiveControlElement);
+backgroundColorBlock.addEventListener('click', handleChangeActiveControlElement);
+fontWeightBlock.addEventListener('click', handleChangeActiveControlElement);
+fontStyleBlock.addEventListener('click', handleChangeActiveControlElement);
